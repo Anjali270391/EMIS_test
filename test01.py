77f109ec5d2c52c767b2308b7dcce1741705184b
Anjali Sinha <104860990+Anjali270391@users.noreply.github.com>
2023-08-14 17:42:06 +0000

    |second commit|

--- test01.py
+++ test01.py
@@ -3,10 +3,7 @@ import pandas as pd
 import numpy as np
 import csv
 
-#pd.read_json(r"C:\Users\riket\Desktop\EMIS_Test\1st_file.json", "r").head()
-
-with open(r"C:\Users\riket\Desktop\EMIS_Test\1st_file.json", "r",  newline='\r\n') as json_file:
-    for line in json_file:
-        data = json.loads(line)
-    ww = pd.DataFrame(data)
-ww.head()
+df = pd.read_json(r"C:\Users\riket\Desktop\EMIS_Test\1st_file.json", "r")
+bn = pd.DataFrame(df.entry.values.tolist())['resource']
+#df.entry.values.tolist()
+pd.DataFrame.from_records(bn).head()
